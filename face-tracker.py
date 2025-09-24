abd30cd14dace0778782379810c1ca06a233a7de
Acanthophrynus747 <jacksbuilds@gmail.com>
2025-09-24 02:01:07 -0700

    improved webcam resolution

--- face-tracker.py
+++ face-tracker.py
@@ -8,51 +8,72 @@ import time
 import os
 import numpy as np
 
-ard = serial.Serial("COM4", 115200) #look into whether different baud rates are better. orginally COM3 but hopefully COM4 will work, COM3 being used by platformio I think?
+usb = serial.Serial("COM4", 115200) #look into whether different baud rates are better. orginally COM3 but hopefully COM4 will work, COM3 being used by platformio I think?
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') #haar cascade file fixed according to https://stackoverflow.com/questions/30508922/error-215-empty-in-function-detectmultiscale
 
-vid = cv2.VideoCapture(0) # webcam 0
+vcap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # webcam 0, idk what cap_dshow does but https://stackoverflow.com/questions/19448078/python-opencv-access-webcam-maximum-resolution
 
-while True:
-    _, frame = vid.read() # current frame to var. frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #frame to greyscale
+#use max possible resolution. maybe not even necessary
+vcap.set(cv2.CAP_PROP_FRAME_WIDTH, 10000)
+vcap.set(cv2.CAP_PROP_FRAME_HEIGHT, 10000) #lol worked, above cap_dshow may not be needed?
 
-    faces = face_cascade.detectMultiScale(gray, minSize=(80, 80), minNeighbors=3)
+try:
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    if vcap.isOpened():
+        vid_width = vcap.get(cv2.CAP_PROP_FRAME_WIDTH) #or width = vcap.get(3)
+        vid_height = vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        fps = vcap.get(cv2.CAP_PROP_FPS)
 
-        x_pos = x + (w/2)
-        y_pos = y + (h/2)
+        print(f"Started video capture window: {vid_width} x {vid_height}px")
 
-        if x_pos > 280:
-            ard.write('L'.encode())
-            time.sleep(0.01)
-        elif x_pos < 360:
-            ard.write('R'.encode())
-            time.sleep(0.01)
-        else:
-            ard.write('S'.encode())
-            time.sleep(0.01)
+    while True:
+        _, frame = vcap.read() # current frame to var. frame
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #frame to greyscale
 
-        if y_pos > 280:
-            ard.write('D'.encode())
-            time.sleep(0.01)
-        elif y_pos < 200:
-            ard.write('U'.encode())
-            time.sleep(0.01)
-        else:
-            ard.write('S'.encode())
-            time.sleep(0.01)
-        break
+        faces = face_cascade.detectMultiScale(gray, minSize=(80, 80), minNeighbors=3)
 
-    cv2.imshow('frame', frame)
-    k = cv2.waitKey(1)&0xFF
-    if(k == ord('q')): #if q is pressed exit while loop
-        break
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-cv2.destroyAllWindows()
-ard.close()
-vid.release()
+            x_pos = x + (w/2) #position of center of face
+            y_pos = y + (h/2)
 
+            x_origin = vid_width / 2
+            y_origin = vid_height / 2 #TODO: add these into below control logic so that it can work with multiple webcam resolutions
+            # if x_pos > x_origin send left or something
+
+            if x_pos > 280:
+                usb.write('L'.encode())
+                time.sleep(0.01)
+            elif x_pos < 360:
+                usb.write('R'.encode())
+                time.sleep(0.01)
+            else:
+                usb.write('S'.encode())
+                time.sleep(0.01)
+
+            if y_pos > 280:
+                usb.write('D'.encode())
+                time.sleep(0.01)
+            elif y_pos < 200:
+                usb.write('U'.encode())
+                time.sleep(0.01)
+            else:
+                usb.write('S'.encode())
+                time.sleep(0.01)
+            break
+
+        # cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+        cv2.imshow('frame', frame)
+
+        k = cv2.waitKey(1)&0xFF
+        if(k == ord('q')): #if q is pressed exit while loop
+            break
+
+    cv2.destroyAllWindows()
+    usb.close()
+    vcap.release()
+
+except KeyboardInterrupt:
+    exit()
